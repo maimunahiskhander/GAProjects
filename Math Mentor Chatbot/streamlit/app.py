50fa6e94dd07a61ee0759ce79d22af1f6f35ff6f
Munah <cloud.dreamer82@gmail.com>
2023-12-06 17:28:34 +0800

    add capstone project

---
+++ Math Mentor Chatbot/streamlit/app.py
@@ -0,0 +1,67 @@
+import streamlit as st
+from llama_index import VectorStoreIndex, ServiceContext, Document, StorageContext, load_index_from_storage
+from llama_index.llms import OpenAI
+import openai
+from PIL import Image
+import random
+
+
+st.set_page_config(page_title="Chat with Your Math Tutor", page_icon="👩🏻‍🏫", layout="centered", initial_sidebar_state="auto", menu_items=None)
+openai.api_key = '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'#st.secrets.openai_key
+
+image = Image.open('st_banner.png')
+st.image(image, caption='Welcome to the AI Math Tutor!',width=1000)
+
+@st.cache_resource(show_spinner=False)
+def load_data():
+    with st.spinner(text="Doing some housekeeping – hang tight! This should take 1-2 minutes."):
+        
+        # Rebuild the storage context
+        storage_context = StorageContext.from_defaults(persist_dir="./data/index.vecstore")
+
+        # Load the index
+        index = load_index_from_storage(storage_context)
+
+        # Load the finetuned model 
+        ft_model_name = "ft:gpt-3.5-turbo-0613:personal::8Qufx558"
+        ft_context = ServiceContext.from_defaults(llm=OpenAI(model=ft_model_name, temperature=0.3), 
+        context_window=2048, 
+        
+        system_prompt="""
+       You are an AI trained to assist with primary school math problems.
+       Give the correct answer and show workings.
+        """
+        )           
+        return index
+
+index = load_data()
+chat_engine = index.as_chat_engine(chat_mode="openai", verbose=True)
+
+if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me a math question 😊"}
+    ]
+
+if prompt := st.chat_input("Your question"):
+    # Save the original user question to the chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.new_question = True
+
+    # Create a detailed prompt for the chat engine
+    chat_history = ' '.join([message["content"] for message in st.session_state.messages])
+    detailed_prompt = f"{chat_history} {prompt}"
+
+if "new_question" in st.session_state.keys() and st.session_state.new_question:
+   for message in st.session_state.messages: # Display the prior chat messages
+       with st.chat_message(message["role"]):
+           st.write(message["content"])
+   st.session_state.new_question = False # Reset new_question to False
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+   with st.chat_message("assistant"):
+       with st.spinner("Thinking..."):
+           response = chat_engine.chat(detailed_prompt)
+           st.write(response.response)
+           # Append the assistant's detailed response to the chat history
+           st.session_state.messages.append({"role": "assistant", "content": response.response})
